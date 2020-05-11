ee46172524f53ec2c45a42e76601be5ed616570e
Nathan Teakle <n.teakle5071@gmail.com>
2020-05-11 12:57:12 +0930

    Update to apiSettings.js

--- settings/apiSettings.js
+++ settings/apiSettings.js
@@ -1,36 +1,35 @@
-const uuidv1 = require('uuid/v1');
 const readDb = require('../db/readDbFile');
 const writeDb = require('../db/writeDbFile');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     const db = readDb;
     res.json(db);
   });
 
 
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function (req, res) {
     const uuid = uuidv1();
     const newNote = req.body;
     newNote.id = uuid;
 
     let db = readDb;
 
-    db.push(newNote); 
+    db.push(newNote);
 
     writeDb(db);
 
     res.json(db);
   });
 
-  app.delete("/api/notes/:id", function(req, res) {
+  app.delete("/api/notes/:id", function (req, res) {
     const db = readDb;
     const deleteId = req.params.id;
 
-    for (let i=0; i<db.length; i+=1 ) {
+    for (let i = 0; i < db.length; i += 1) {
       if (db[i].id === deleteId) {
-        db.splice(i,1);
+        db.splice(i, 1);
         break;
       }
     }
