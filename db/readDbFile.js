d81282e41b771e702a5b2dfb7422b66c42545b8b
Nathan Teakle <n.teakle5071@gmail.com>
2020-05-11 13:03:29 +0930

    Update to readDbFile.js

--- db/readDbFile.js
+++ db/readDbFile.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-
 const filename = "./db.json";
 
-const dbfile = JSON.parse(fs.readFileSync(path.join(__dirname, filename),(err, data) => {
+const dbfile = JSON.parse(fs.readFileSync(path.join(__dirname, filename), (err, data) => {
     if (err) throw err;
 }));
 
